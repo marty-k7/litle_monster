6583f0b3ce119045455a91c834121702533e24b9
marty.k7 <martynas.klastaitis@gmail.com>
2016-04-08 00:39:47 +0300

    Initial Commit

---
+++ Litle_MonsterTests/Litle_MonsterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Litle_MonsterTests.swift
+//  Litle_MonsterTests
+//
+//  Created by Martynas Klastaitis  on 08/04/16.
+//  Copyright © 2016 marty.k7. All rights reserved.
+//
+
+import XCTest
+@testable import Litle_Monster
+
+class Litle_MonsterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
