f76e87bebd0f2f1e2ad825664e4ab4e1101ebe41
marty.k7 <martynas.klastaitis@gmail.com>
2016-04-08 03:40:19 +0300

    added first day work

---
+++ Litle_Monster/DragImg.swift
@@ -0,0 +1,19 @@
+//
+//  DragImg.swift
+//  Litle_Monster
+//
+//  Created by Martynas Klastaitis  on 08/04/16.
+//  Copyright © 2016 marty.k7. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DragImg: UIImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
--- Litle_Monster/ViewController.swift
+++ Litle_Monster/ViewController.swift
@@ -9,15 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: UIImageView!
+    @IBOutlet weak var heartImg: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        //sukuriam aray[UIImage] ir i ja dedam kiekviena paveiksliuka pagal numerius
+        var imgAray = [UIImage]()
+        for var x = 1; x <= 4; x += 1 {
+            let img = UIImage(named: "idle\(x).png")
+            imgAray.append(img!)
+        }
+        
+        monsterImg.animationImages = imgAray //is kur imam paveiksliukus
+        monsterImg.animationDuration = 0.8  //animacijos greitis arba kiek trunka
+        monsterImg.animationRepeatCount = 0 //kiek kartu pasikartoja, 0 = kartojasi nesustodama
+        monsterImg.startAnimating() //pradedam animuoti
+        
+        
+        
     }
 
 
